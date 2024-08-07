d0957669d175a35df9ce0497a0c1f833cf70e933
Rijan-Joshi <csaijspy@gmail.com>
2024-08-07 12:32:45 +0545

    Setting up Next.js and tailwind css to build the landing page

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/lib/utils'
+import React from 'react'
+
+const MaxWidthWrapper = (
+    { className, children }:
+    {
+        className?: String,
+        children : React.ReactNode
+    }
+) => {
+  return (
+    <div className={cn("mx-auto w-full px-2.5 md:pd-20", className)} >
+          {children}
+    </div>
+  )
+}
+
+export default MaxWidthWrapper
